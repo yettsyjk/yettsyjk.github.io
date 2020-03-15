3da40cb8437f595c343f6378657bc11f153ec2e6
yettsyjk <56506858+yettsyjk@users.noreply.github.com>
2020-03-15 12:29:17 -0600

    new personal.css

---
+++ js/bulma.js
@@ -0,0 +1,8 @@
+(function() {
+    let burger = document.querySelector('.burger');
+    let menu = document.querySelector('#'+ burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+}) () ;
